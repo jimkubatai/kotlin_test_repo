a04a27c1d79ee03c9c838c3cb738690317455f42
Dmitriy Chernyakov <kubatai@rocketmail.com>
2021-04-07 20:05:40 +0300

    Работа с тестами

--- lesson6/src/main/kotlin/Car.kt
+++ lesson6/src/main/kotlin/Car.kt
@@ -1,8 +1,3 @@
 data class Car (var id: Int, val licensePlate: String, val fuelCardNumbers: List<String>) {
-    fun isContainsFuelCard(carNumber: String) : Boolean {
-        return fuelCardNumbers.contains(carNumber)
-    }
-
-    fun car (block: Car.() -> Unit) : Unit = block(this)
-
+    fun car (block: Car.() -> Car) : Car  = this.block()
 }
--- lesson6/src/test/kotlin/CarTest.kt
+++ lesson6/src/test/kotlin/CarTest.kt
@@ -1,22 +1,29 @@
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.mockkObject
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class CarTest {
 
     @Test
     fun car() {
-        repeat(10) { operandId ->
-            val car = mockk<Car>()
+          repeat(10) { id ->
 
-            assert(operandId + 1 == car.id)
-        }
+              val car = Car(id, "", listOf())
 
+              mockkObject(car)
+
+              every {  car.car {
+                  this.id = this.id + 1
+                  this
+              } } returns Car(id + 1, "", listOf())
+
+
+              assertEquals(id + 1, car.car {
+                  this.id = this.id + 1
+                  this
+              } .id )
+          }
     }
-
-    fun incCarId (it: Car): Car {
-        it.id = it.id + 1
-        return it
-    }
-
 }
