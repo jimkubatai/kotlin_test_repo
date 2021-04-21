6144215ffdf9806869bbe4800cee52b4e8c16a5c
Dmitriy Chernyakov <kubatai@rocketmail.com>
2021-04-21 19:23:28 +0300

    Реализация задачи

--- lesson8/src/main/kotlin/main.kt
+++ lesson8/src/main/kotlin/main.kt
@@ -1,8 +1,52 @@
-fun main() {
-    val totalCars = CarsData()
-    val totalCards = FuelCardsData()
-    val service = Service(totalCards, totalCars)
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
 
-    println(service.getCarLicensePlate(1))
-    println(service.isCarExist(1))
+suspend fun main() {
+
+    var totalCars = listOf<Car>()
+    var totalCards = listOf<FuelCard>()
+    val job1 = GlobalScope.launch {
+        delay(1000L)
+        totalCars = CarsData().getAll()
+    }
+
+    val job2 = GlobalScope.launch {
+        delay(1500L)
+        totalCards = FuelCardsData().getAll()
+    }
+
+    job1.join()
+    job2.join()
+
+    println(totalCars)
+    println(totalCards)
+
+    val channel = Channel<FuelCard>()
+
+    val job3 = GlobalScope.launch {
+        while (this.isActive) {
+            for(card in totalCards) {
+                delay(5000L)
+                channel.send(card)
+                println("Sent card $card")
+            }
+        }
+    }
+
+    val job4 = GlobalScope.launch {
+        while (this.isActive) {
+            for(card in totalCards) {
+                delay(1500L)
+                val card = channel.receive()
+                println("Received card $card")
+            }
+        }
+    }
+
+    delay(15000L)
+    println("Time off")
+    job3.cancel()
+    job4.cancel()
+
+    println("Exit")
 }
